9ca3315bce2e9223e1ae54a28470f8556339e775
Jeremy Thaller <32827440+jthaller@users.noreply.github.com>
2020-08-23 06:49:56 -0400

    Add files via upload

--- epma_analysis.py
+++ epma_analysis.py
@@ -1,5 +1,8 @@
+# Jeremy Thaller
+# Aug 19, 2020 
 # Turn EPMA weight percentages into atomic compositions
-# I've set this up to run as interactive code is VS Code 
+# The # %% between sections exists because I've set this up 
+# to run as interactive code is VS Code 
 
 
 # Import the spreadsheet and split into 2 dataframes
@@ -21,7 +24,7 @@ cat_wts = {"NA": 22.989769,
            "MG": 24.305,
            "SI": 28.0855,
            "AL": 26.981539,
-           "P": 30.973762,
+           "P":  30.973762,
            "K":  39.0983,
            "CA": 40.078,
            "TI": 47.867,
@@ -31,26 +34,27 @@ cat_wts = {"NA": 22.989769,
            "S":  32.065,
            "CL": 35.453}
 
-
+'''
+Takes a slice of the dataframe and returns an array with the mean weight
+of each oxide, normalized so that the total of all weight percents = 100%
+Inputs: dataframe with each column as oxide wt percent, and rows as trial num
+Outputs: array of wt percents
+'''
 #%%
 def mean_and_normalize(oxides_df):
     oxides_df = oxides_df.drop(oxides_df.columns[0:2], axis=1)
+    #create new row = mean
     oxides_df.loc['mean'] = oxides_df.mean()
+    #get rid of everything other than oxide weights
     oxides_df = oxides_df.loc['mean'][0:14].values
+    #normalize so percentages add up to 100% total
     return [grain/oxides_df[-1] *100 for grain in oxides_df]
 
 #%% # GRAIN 1 NIGHTTIME LINE
-# grain_1_ln = df_nline.drop(df.columns[0:2], axis=1)
-# #create new row = mean
-# grain_1_ln.loc['mean'] = grain_1_ln.mean()
-# #get rid of everything other than oxide weights
-# grain_1_ln = grain_1_ln.loc['mean'][0:14].values
-# #normalize so percentages add up to 100% total
-# grain_1_ln = [grain/grain_1_ln[-1] *100 for grain in grain_1_ln]
 grain_1_ln = mean_and_normalize(df_nline)
 print(grain_1_ln)
 
-#%% # GRAIN 1 DAYTIME POINTS
+#%% # DAYTIME, SCATTER POINTS
 grain_1_pts = mean_and_normalize(df_dpoints[0:5])
 grain_2 = mean_and_normalize(df_dpoints[5:8])
 grain_3 = mean_and_normalize(df_dpoints[8:13])
@@ -73,7 +77,6 @@ print(df_dpoints[16:20].info)
 
 # %%
 #list of namesin the same order as above
-# cats = ["NA", "MG", "SI", "AL", "K", "CA", "TI", "FE", "MN"]
 cats = [cat for cat  in cat_wts]
 # In the order above, eg [(Na2O), (MgO), etc.]
 oxide_nums = [(2,1),(1,1), (1,2), (2,3), (2,5), (2,1), (1,1), (1,2), (1,1), (2,3), (1,1), (1,3), (1,0)]
@@ -83,7 +86,7 @@ converts sample's weight percents to mols of each atom
 inputs: cat_weight = dict {key='atom': val=amu weight}
         num_cat = int = number of cations in the oxide e.g. 1 for SiO2
         num_o = int = number of oxygens in oxide e.g. 2 for SiO2
-        weight_percent = array of floats [weight percetages of each oxide measured via EPMA]
+        weight_percent = array of floats [weight percentages of each oxide measured via EPMA]
 returns: dict {key=atom: val=mols in sample}
 '''
 def convert(cat_weight, num_cat, num_o, weight_percent):
@@ -102,7 +105,6 @@ def calc_mols(sample):
     samp_mols = {"O": 0}
     for i in range(0, len(sample)-1):
         cat, o = convert(cat_wts[cats[i]], oxide_nums[i][0], oxide_nums[i][1], sample[i])
-        # cat, o = convert(cat_wts[cats[i]], num_cats[i], num_o[i], sample[i])
         samp_mols[cats[i]] = cat
         samp_mols["O"] += o
     return samp_mols
@@ -139,7 +141,7 @@ def atomic_nums(samp, samp_num=0, num_O=6):
     #now normalize and print
     print(f"\n---Grain: {samp_num} Normalized---\nAtom\tMols")
     for atom, mol in samp.items():
-        norm = (1 / samp["O"])*num_O
+        norm = (num_O / samp["O"])
         mol = mol*norm
         print('{}\t{}'.format(atom, round(mol,2)))
 
@@ -149,9 +151,9 @@ def atomic_nums(samp, samp_num=0, num_O=6):
 # atomic_nums(grain_2_mols, 2, 8)
 atomic_nums(grain_10_mols, 10, 8)
 
-%##
+# %%
 # -----RESULTS-----
-"""
+'''
 Grain_1_ln = NaAlSi3O8 but with less Na = Albite. Small Ca inclusion
 Grain_1_pts =  KAlSi3O8 = Orthoclase. But not as much K measured and a modicum of Na inclusion.
 Grain_2 = KAlSi3O8 = Orthoclase. Basically the same as Grain_1_pts. I think he screwed up the labeling.
@@ -170,4 +172,4 @@ Grain_9 = KAlSi3O8 = Orthoclase. But less Si, .4K, .15 Ca, .05 Ti, and tiny bit 
 Maybe redo grain 9 because it's weird and the weight percentages only added up to 69%
 Grain_10 = SiO2 = Quartz. But with a modicum of Fe and Cr inclusions.
 grain 10 data only had total weight percent of 36%. Not sure if this is reliable data
-"""
+'''
